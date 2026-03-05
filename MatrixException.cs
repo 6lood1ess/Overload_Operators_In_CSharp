9568b490232977a6cda86f3e386044bbcb899a1b
6lood1ess <6lo.od1ess@gmail.com>
2026-03-05 10:06:46 +0700

    Add custom MatrixException class for error handling

---
+++ MatrixException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MatrixCalculator {
+
+  // Custom exception for matrix operations
+  public class MatrixException : Exception {
+    public MatrixException(string message) : base(message) { }
+  }
+}
