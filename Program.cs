ec130c18f3dc8ecee9bcf5c1477584727779a61b
6lood1ess <6lo.od1ess@gmail.com>
2026-03-05 13:05:06 +0700

    Add MatrixApplication class and simple test to demonstrate matrix operations

--- Program.cs
+++ Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MatrixCalculator
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
+namespace MatrixCalculator {
+
+  class MatrixApplication {
+
+    private Matrix _matrixA;
+    private Matrix _matrixB;
+
+    public void Run() {
+
+      try {
+
+        Console.WriteLine("=== MATRIX CALCULATOR ===\n");
+
+        _matrixA = new Matrix(3);
+        _matrixB = new Matrix(3);
+
+        Console.WriteLine("Matrix A:" +
+                          $"{_matrixA}\n" +
+                          "Matrix B:" +
+                          $"{_matrixB}\n" +
+                          "A + B:" +
+                          $"{ _matrixA + _matrixB}\n" +
+                          "A * B:" +
+                          $"{_matrixA * _matrixB}\n" +
+                          $"Determinant of A: {_matrixA.CalculateDeterminant()}");
+      }
+
+      catch (Exception exception) {
+        Console.WriteLine($"Error: {exception.Message}");
+      }
     }
+  }
+
+  class Program {
+
+    static void Main(string[] args) {
+
+      MatrixApplication app = new MatrixApplication();
+
+      app.Run();
+      Console.ReadKey();
+    }
+  }
 }
