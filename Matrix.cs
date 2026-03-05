aa819bcc5a1f1918cd129fd55f1f6f024517a710
6lood1ess <6lo.od1ess@gmail.com>
2026-03-05 10:24:23 +0700

    Add Matrix class with constructors for random generation and copying

---
+++ Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MatrixCalculator {
+  
+  public class Matrix : ICloneable, IComparable<Matrix> {
+
+    private int[,] _elements;
+    private int _size;
+    private Random _randomGenerator;
+
+    // Constructor for random generation
+    public Matrix(int size) {
+
+      if (size <= 0) {
+        throw new MatrixException("Matrix size must be positive");
+      }
+
+      _randomGenerator = new Random();
+      _size = size;
+      _elements = new int[_size, _size];
+
+      for (int rowIndex = 0; rowIndex < _size; ++rowIndex) {
+        for (int columnIndex = 0; columnIndex < _size; ++columnIndex) {
+          _elements[rowIndex, columnIndex] = _randomGenerator.Next(1, 10);
+        }
+      }
+    }
+
+    // Copy constructor
+    public Matrix(Matrix otherMatrix) {
+
+      if (otherMatrix == null) {
+        throw new MatrixException("Source matrix cannot be null");
+      }
+
+      _randomGenerator = new Random();
+      _size = otherMatrix._size;
+      _elements = new int[_size, _size];
+
+      for (int rowIndex = 0; rowIndex < _size; ++rowIndex) {
+        for (int columnIndex = 0; columnIndex < _size; ++columnIndex) {
+          _elements[rowIndex, columnIndex] = otherMatrix._elements[rowIndex, columnIndex];
+        }
+      }
+    }
+  }
+}
